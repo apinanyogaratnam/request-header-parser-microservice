eb81e7867abbaf46bbea35819555d21684f98336
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-10-05 20:13:53 -0400

    created get request call without implementation details

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami/", function(req, res) {
+  
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
